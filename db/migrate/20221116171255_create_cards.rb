ab54732eea7c21aacd52a345f3fed3ef817418a2
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-11-16 14:25:29 -0300

    create card table

---
+++ db/migrate/20221116171255_create_cards.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/MethodLength
+
+class CreateCards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cards do |t|
+      t.string :type, null: false
+      t.string :name_short, null: false
+      t.string :name, null: false
+      t.string :value, null: false
+      t.integer :value_int, null: false
+      t.string :meaning_up, null: false
+      t.string :meaning_rev, null: false
+      t.string :desc, null: false
+      t.string :suit
+
+      t.timestamps
+    end
+  end
+end
