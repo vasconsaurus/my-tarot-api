6a86ba0fcac39f2b5a5f0db44a7aeabf43ce950c
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-11-16 14:05:01 -0300

    renamed app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "tarot_api_production"
+  # config.active_job.queue_name_prefix = "my_tarot_api_production"
 
   config.action_mailer.perform_caching = false
 
