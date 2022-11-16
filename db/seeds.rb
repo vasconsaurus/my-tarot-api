3bd401fdac2126c65e6ba8728bc76acd08f4ed49
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-11-16 15:03:38 -0300

    removed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,3 @@ parsed_cards = JSON.parse(File.read(Rails.root.join('db/card_data.json')))
 parsed_cards.each do |card|
   Card.create!(card)
 end
-
-  # Card.create!(
-  #   name_short: 'test',
-  #   name: 'test',
-  #   value: 'test',
-  #   value_int: 1,
-  #   meaning_up: 'test',
-  #   meaning_rev: 'test',
-  #   card_description: 'test',
-  #   type_of_card: 'test'
-  # )
