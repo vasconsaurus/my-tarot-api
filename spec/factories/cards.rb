2e92d17f9cb473fbe6089f2ccf364d8b62e5db6d
Manu Vasconcelos <87862340+vasconsaurus@users.noreply.github.com>
2022-11-18 10:11:30 -0300

    feat: add CI pipeline using GH Actions (#2)
    * added workflows for rspec, brakeman and rubocop
    * had to fix rubocop hell, everything seems to be working now,
    but this could be a source of bugs (so I should keep an eye on this)

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :card do
-    type { "" }
-    name_short { "MyString" }
-    name { "MyString" }
-    value { "MyString" }
+    type { '' }
+    name_short { 'MyString' }
+    name { 'MyString' }
+    value { 'MyString' }
     value_int { 1 }
-    meaning_up { "MyString" }
-    meaning_rev { "MyString" }
-    desc { "MyString" }
+    meaning_up { 'MyString' }
+    meaning_rev { 'MyString' }
+    desc { 'MyString' }
   end
 end
